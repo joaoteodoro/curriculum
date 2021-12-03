429ae99b93315a51c2f4368814247b5fc1a11a4e
joaoteodoro <joaoneto.itba@gmail.com>
2021-12-02 22:36:32 -0300

    update: atualizando informações

---
+++ src/database/en.ts
@@ -0,0 +1,127 @@
+export const informationEn = {
+  details: {
+    name: 'João Teodoro de Oliveira Neto',
+    jobTitle: 'Software Developer',
+    phone: '+5534992316267',
+    email: 'joaoneto.itba@gmail.com',
+  },
+  links: [
+    {
+      url: 'https://github.com/joaoteodoro',
+      name: 'Github'
+    },
+    {
+      url: 'https://www.linkedin.com/in/jo%C3%A3o-teodoro-47178b75/',
+      name: 'Linkedin'
+    }
+  ],
+  skills: [
+    {
+      name: 'Java',
+      level: 4,
+    },
+    {
+      name: 'Spring Framework',
+      level: 4,
+    },
+    {
+      name: 'Javascript',
+      level: 4,
+    },
+    {
+      name: 'HTML',
+      level: 4,
+    },
+    {
+      name: 'CSS',
+      level: 4,
+    },
+    {
+      name: 'Angular',
+      level: 4,
+    },
+    {
+      name: 'Git',
+      level: 4,
+    },
+    {
+      name: 'Node.js',
+      level: 3,
+    },
+    {
+      name: 'React',
+      level: 3,
+    },
+    {
+      name: 'SQL',
+      level: 3,
+    },
+    {
+      name: 'AWS',
+      level: 2,
+    },
+  ],
+  profile: {
+    description: "6 years of experience in software development with greater knowledge in Java and Javascript. Graduated in Information Systems from the Federal University of Uberlândia. I also have knowledge of SQL database. I'm currently working as a fullstack developer, using Angular for frontend and Java with Spring modules for backend. I have an intermediate level of English.",
+  },
+  employments: [
+    {
+      title: 'Trainee',
+      employer: 'Powerlogic',
+      startDate: 'Jun 2015',
+      endDate: 'Sep 2015',
+      city: 'Uberlândia',
+      description: 'I worked in the development and maintenance of systems linked to Uberlândia city council, working with database modeling and Java EE application development with JSF. Tools used: Java EE, JSF, Oracle Database, Hibernate, MVC.'
+    },
+    {
+      title: 'Junior Developer',
+      employer: 'Ibrowse Consultoria e Informática',
+      startDate: 'Oct 2015',
+      endDate: 'Jan 2016',
+      city: 'Uberlândia',
+      description: 'I worked in the development and maintenance of systems linked to Uberlândia city council, working with database modeling and Java EE application development with JSF. Tools used: Java EE, JSF, Oracle Database, Hibernate, MVC.'
+    },
+    {
+      title: 'Junior Developer',
+      employer: 'everis',
+      startDate: 'Feb 2016',
+      endDate: 'Aug 2016',
+      city: 'Uberlândia',
+      description: 'I worked in the development and maintenance of systems for companies in the telephony area, working mainly with web systems. Tools used: Java EE, JSP, Spring, Hibernate, Oracle Database, Bootstrap.'
+    },
+    {
+      title: 'Developer',
+      employer: 'Ibrowse Consultoria e Informática',
+      startDate: 'Sep 2016',
+      endDate: 'Jan 2018',
+      city: 'Uberlândia',
+      description: 'I worked in the development and maintenance of systems linked to Uberlândia city council, mainly in the migration of Java EE systems with JSF to applications divided into frontend and backend (REST API), using Angular and Sringboot respectively. Tools used: Java EE, JSF, Spring, REST, Hibernate, Oracle Database, Bootstrap, Angular, HTML, CSS, Javascript, Typescript.'
+    },
+    {
+      title: 'Senior Developer',
+      employer: 'Ene Soluções',
+      startDate: 'Jan 2018',
+      endDate: 'May 2020',
+      city: 'Uberlândia',
+      description: 'I worked in the development and maintenance of web systems in several areas: telephony, system integrators, ad sites. In several projects that I participated, I worked with database, with backend applications and also with frontend applications. Tools used: Java, Spring, REST, Hibernate, RestTemplate, Node.js, NestJS, Oracle Database, PostgresSQL and MySQL, Angular, React, HTML, CSS, Javascript, Typescript.'
+    },
+    {
+      title: 'Specialist Developer',
+      employer: 'everis',
+      startDate: 'May 2020',
+      endDate: '',
+      city: 'Uberlândia',
+      description: 'I currently work in the development and maintenance of web systems for the banking sector. I also support and mentor other developers working on the same systems. Tools used: Java, Spring, JSP, MVC, REST, Webtarget, JUnit, Mockito, Angular, HTML, CSS, Javascript, Typescript.'
+    },
+  ],
+  education: [
+    {
+      title: 'Information Systems',
+      employer: 'Federal University of Uberlândia',
+      startDate: 'Mar 2012',
+      endDate: 'Feb 2017',
+      city: 'Uberlândia',
+      description: ''
+    },
+  ]
+}
---
+++ src/database/pt-br.ts
@@ -0,0 +1,128 @@
+export const informationPtBr = {
+  details: {
+    name: 'João Teodoro de Oliveira Neto',
+    jobTitle: 'Desenvolvedor de Sistemas',
+    phone: '+5534992316267',
+    email: 'joaoneto.itba@gmail.com',
+  },
+  links: [
+    {
+      url: 'https://github.com/joaoteodoro',
+      name: 'Github'
+    },
+    {
+      url: 'https://www.linkedin.com/in/jo%C3%A3o-teodoro-47178b75/',
+      name: 'Linkedin'
+    }
+  ],
+  skills: [
+    {
+      name: 'Java',
+      level: 4,
+    },
+    {
+      name: 'Spring Framework',
+      level: 4,
+    },
+    {
+      name: 'Javascript',
+      level: 4,
+    },
+    {
+      name: 'HTML',
+      level: 4,
+    },
+    {
+      name: 'CSS',
+      level: 4,
+    },
+    {
+      name: 'Angular',
+      level: 4,
+    },
+    {
+      name: 'Git',
+      level: 4,
+    },
+    {
+      name: 'Node.js',
+      level: 3,
+    },
+    {
+      name: 'React',
+      level: 3,
+    },
+    {
+      name: 'SQL',
+      level: 3,
+    },
+    {
+      name: 'AWS',
+      level: 2,
+    },
+  ],
+  profile: {
+    description: "6 anos de experiência em desenvolvimento de softwares com maiores conhecimentos em Java e Javascript. Graduado em Sistemas de Informação pela Universidade Federal de Uberlândia. Também tenho conhecimento em banco de dados SQL. Atualmente estou trabalhando como desenvolvedor fullstack, usando as ferramentas Angular (frontend) e Java com módulos spring (backend). Tenho nível de inglês intermediário.",
+  },
+  employments: [
+    {
+      title: 'Estagiário',
+      employer: 'Powerlogic',
+      startDate: 'Jun 2015',
+      endDate: 'Set 2015',
+      city: 'Uberlândia',
+      description: 'Trabalhei no desenvolvimento e manutenção de sistemas vinculados a prefeitura de Uberlândia, atuando com modelagem de banco de dados e desenvolvimento de aplicações Java EE com JSF. Ferramentas utilizadas: Java EE, JSF, Banco de Dados Oracle, Hibernate, MVC.'
+    },
+    {
+      title: 'Desenvolvedor Júnior',
+      employer: 'Ibrowse Consultoria e Informática',
+      startDate: 'Out 2015',
+      endDate: 'Jan 2016',
+      city: 'Uberlândia',
+      description: 'Trabalhei no desenvolvimento e manutenção de sistemas vinculados a prefeitura de Uberlândia, atuando com modelagem de banco de dados e desenvolvimento de aplicações Java EE com JSF. Ferramentas utilizadas: Java EE, JSF, Banco de Dados Oracle, Hibernate, MVC.'
+    },
+    {
+      title: 'Desenvolvedor Júnior',
+      employer: 'everis',
+      startDate: 'Fev 2016',
+      endDate: 'Ago 2016',
+      city: 'Uberlândia',
+      description: 'Trabalhei no desenvolvimento e manutenção de sistemas de empresas da área de telefonia, atuando principalmente com sistemas web. Ferramentas utilizadas: Java EE, JSP, Spring, Hibernate, Banco de dados Oracle, Bootstrap.'
+    },
+    {
+      title: 'Desenvolvedor Pleno',
+      employer: 'Ibrowse Consultoria e Informática',
+      startDate: 'Set 2016',
+      endDate: 'Jan 2018',
+      city: 'Uberlândia',
+      description: 'Trabalhei no desenvolvimento e manutençao de sistemas vinculados a prefeitura de Uberlândia, principalmente na migração de sistemas Java EE com JSF para aplicações divididas em frontend e backend (API REST), utilizando Angular e Sringboot respectivamente. Ferramentas utilizadas: Java EE, JSF, Spring, REST, Hibernate, Banco de dados Oracle, Bootstrap, Angular, HTML, CSS, Javascript, Typescript.'
+    },
+    {
+      title: 'Desenvolvedor Sênior',
+      employer: 'Ene Soluções',
+      startDate: 'Jan 2018',
+      endDate: 'Mai 2020',
+      city: 'Uberlândia',
+      description: 'Trabalhei no desenvolvimento e manutenção de sistemas web de diversas áreas: telefonia, integradores de sistemas, sites de anúncios. Nos vários projetos que participei, atuei com banco de dados, com aplicações backend e com aplicações frontend também. Ferramentas utilizadas: Java, Spring, REST, Hibernate, RestTemplate, Node.js, NestJS, Banco de dados Oracle, PostgresSQL e MySQL, Angular, React, HTML, CSS, Javascript, Typescript.'
+    },
+    {
+      title: 'Desenvolvedor Especialista',
+      employer: 'everis',
+      startDate: 'Mai 2020',
+      endDate: '',
+      city: 'Uberlândia',
+      description: 'Atualmente trabalho no desenvolvimento e manutenção de sistemas web do setor bancário. Também apoio e faço mentoria a outros desenvolvedores que atuam no mesmos sistemas. Ferramentas utilizadas: Java, Spring, JSP, MVC, REST, Webtarget, JUnit, Mockito, Angular, HTML, CSS, Javascript, Typescript.'
+    },
+  ],
+  education: [
+    {
+      title: 'Sistemas de Informação',
+      employer: 'Universidade Federal de Uberlândia',
+      startDate: 'Mar 2012',
+      endDate: 'Fev 2017',
+      city: 'Uberlândia',
+      description: ''
+    },
+  ]
+
+}
